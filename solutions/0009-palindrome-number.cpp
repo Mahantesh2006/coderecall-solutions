884772b0b92ea8c8a747d774de8607313fe871c8
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 06:44:03 +0530

    feat: add LeetCode #9 Palindrome Number (cpp)

---
+++ solutions/0009-palindrome-number.cpp
@@ -0,0 +1,22 @@
+// ////////////////////////////////////////////////////////////
+// LeetCode #9: Palindrome Number
+// Difficulty : EASY
+// Pattern    : Math
+// Time       : $O(\log_{10}(N))$ logarithmic time.
+// Space      : $O(1)$ constant auxiliary space.
+// URL        : https://leetcode.com/problems/palindrome-number/
+// Synced via : CodeRecall (https://code-recall-one.vercel.app)
+// ////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
