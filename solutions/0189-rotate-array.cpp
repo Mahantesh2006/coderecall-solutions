18c81a645db1c14619f3ae671719ece5189f357a
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 08:58:35 +0530

    feat: add LeetCode #189 Rotate Array (cpp)

--- solutions/0189-rotate-array.cpp
+++ solutions/0189-rotate-array.cpp
@@ -8,20 +8,24 @@
 // Synced via : CodeRecall (https://code-recall-one.vercel.app)
 // ////////////////////////////////////////////////////////////
 
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int rotateArray(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += nums[i];
+    void rotate(vector<int>& arr, int k) {
+        int n=arr.size();
+        if(n==0){
+            return;
         }
-        return ans;
+        
+        k=k%n;
+        vector<int> temp(k);
+        for(int i=n-k;i<n;i++){
+            temp[i-n+k]=arr[i];
+        }
+        for(int i=n-k-1;i>=0;i--){
+            arr[i+k]=arr[i];
+        }
+        for(int i=0;i<k;i++){
+            arr[i]=temp[i];
+        }
     }
 };
