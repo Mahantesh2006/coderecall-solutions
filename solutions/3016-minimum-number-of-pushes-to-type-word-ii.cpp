8264b6ed71e9f0ff8071c7bcac507ca927e1ad35
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 07:35:36 +0530

    feat: add LeetCode #3016 Minimum Number of Pushes to Type Word II (cpp)

---
+++ solutions/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,31 @@
+// ////////////////////////////////////////////////////////////
+// LeetCode #3016: Minimum Number of Pushes to Type Word II
+// Difficulty : MEDIUM
+// Pattern    : Hash Table
+// Time       : $O(N)$ linear time to count frequencies + $O(1)$ sorting of 26 letters.
+// Space      : $O(1)$ constant auxiliary space for 26 frequency buckets.
+// URL        : https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-ii/
+// Synced via : CodeRecall (https://code-recall-one.vercel.app)
+// ////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for(char c : word)
+            freq[c - 'a']++;
+
+        sort(freq.begin(), freq.end(), greater<int>());
+
+        int ans = 0;
+
+        for(int i = 0; i < 26; i++) {
+            if(freq[i] == 0) break;
+
+            ans += freq[i] * (i / 8 + 1);
+        }
+
+        return ans;
+    }
+};
