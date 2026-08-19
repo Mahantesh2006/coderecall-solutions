3253b93aae4a2189edff09058f589007eebc9d6e
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 08:58:02 +0530

    feat: add LeetCode #7 Reverse Integer (cpp)

--- solutions/0007-reverse-integer.cpp
+++ solutions/0007-reverse-integer.cpp
@@ -8,20 +8,26 @@
 // Synced via : CodeRecall (https://code-recall-one.vercel.app)
 // ////////////////////////////////////////////////////////////
 
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int reverseInteger(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += nums[i];
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int ld = x % 10;
+            x /= 10;
+
+            // Check for overflow
+            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && ld > 7))
+                return 0;
+
+            // Check for underflow
+            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && ld < -8))
+                return 0;
+
+            rev = rev * 10 + ld;
         }
-        return ans;
+
+        return rev;
     }
 };
