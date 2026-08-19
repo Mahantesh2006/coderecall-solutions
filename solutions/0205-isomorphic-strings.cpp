eda000c690b5bbd583a6b4a4ea293bc95ae91f05
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 07:33:48 +0530

    feat: add LeetCode #205 Isomorphic Strings (cpp)

---
+++ solutions/0205-isomorphic-strings.cpp
@@ -0,0 +1,34 @@
+// ////////////////////////////////////////////////////////////
+// LeetCode #205: Isomorphic Strings
+// Difficulty : EASY
+// Pattern    : Hash Table
+// Time       : O(N) - Linear time pass through input elements.
+// Space      : O(1) - Constant auxiliary space.
+// URL        : https://leetcode.com/problems/isomorphic-strings/
+// Synced via : CodeRecall (https://code-recall-one.vercel.app)
+// ////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> mp1, mp2;
+
+        for (int i = 0; i < s.length(); i++) {
+            char a = s[i];
+            char b = t[i];
+
+            // s -> t mapping
+            if (mp1.count(a) && mp1[a] != b)
+                return false;
+
+            // t -> s mapping
+            if (mp2.count(b) && mp2[b] != a)
+                return false;
+
+            mp1[a] = b;
+            mp2[b] = a;
+        }
+
+        return true;
+    }
+};
