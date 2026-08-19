b56f58ea9e547ef595f0e5996d2c0022eb8a973e
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 06:44:09 +0530

    feat: add LeetCode #1 Two Sum (cpp)

---
+++ solutions/0001-two-sum.cpp
@@ -0,0 +1,26 @@
+// ////////////////////////////////////////////////////////////
+// LeetCode #1: Two Sum
+// Difficulty : EASY
+// Pattern    : Array
+// Time       : $O(N)$ linear time complexity.
+// Space      : $O(N)$ linear auxiliary space for hash map.
+// URL        : https://leetcode.com/problems/two-sum/
+// Synced via : CodeRecall (https://code-recall-one.vercel.app)
+// ////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+            if (mp.count(diff)) return {mp[diff], i};
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
