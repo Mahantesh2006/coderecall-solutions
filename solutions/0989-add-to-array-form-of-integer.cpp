2e95ffdca40d7760c4b723fe70e65f3ee1852b11
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-08-19 09:13:09 +0530

    feat: add LeetCode #989 Add to Array-Form of Integer (cpp)

---
+++ solutions/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,31 @@
+// ////////////////////////////////////////////////////////////
+// LeetCode #989: Add to Array-Form of Integer
+// Difficulty : EASY
+// Pattern    : Array
+// Time       : O(N) - Linear time pass through input elements.
+// Space      : O(1) - Constant auxiliary space.
+// URL        : https://leetcode.com/problems/add-to-array-form-of-integer/
+// Synced via : CodeRecall (https://code-recall-one.vercel.app)
+// ////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        int i = num.size() - 1;
+
+        while (i >= 0 && k > 0) {
+            int sum = num[i] + k;
+
+            num[i] = sum % 10;
+            k = sum / 10;
+            i--;
+        }
+
+        while (k > 0) {
+            num.insert(num.begin(), k % 10);
+            k /= 10;
+        }
+
+        return num;
+    }
+};
