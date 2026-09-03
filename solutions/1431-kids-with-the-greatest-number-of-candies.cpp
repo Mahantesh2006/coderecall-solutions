5bd852d62e4a76f2c0ee3e9085c52125917e1dda
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-09-04 00:27:32 +0530

    feat: add LeetCode #1431 Kids With the Greatest Number of Candies (cpp)

--- solutions/1431-kids-with-the-greatest-number-of-candies.cpp
+++ solutions/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -8,20 +8,17 @@
 // Synced via : CodeRecall (https://coderecall.vercel.app)
 // ////////////////////////////////////////////////////////////
 
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
-    int kidsWithTheGreatestNumberOfCandies(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            ans += nums[i];
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n=candies.size();
+        vector<bool> result(n,false);
+        int maxx=*max_element(candies.begin(),candies.end());
+        for(int i=0;i<n;i++){
+            if(candies[i]+extraCandies>=maxx){
+                result[i]=true;
+            }
         }
-        return ans;
+        return result;
     }
 };
