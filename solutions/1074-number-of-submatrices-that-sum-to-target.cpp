22ee66bd080349bfe1a1c369faa137223ead2158
Mahantesh2006 <159122241+Mahantesh2006@users.noreply.github.com>
2026-09-08 01:09:34 +0530

    feat: add LeetCode #1074 Number of Submatrices That Sum to Target (cpp)

--- solutions/1074-number-of-submatrices-that-sum-to-target.cpp
+++ solutions/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -2,10 +2,46 @@
 // LeetCode #1074: Number of Submatrices That Sum to Target
 // Difficulty : HARD
 // Pattern    : Array
-// Time       : O(N) - Linear time pass through input elements.
-// Space      : O(1) - Constant auxiliary space.
+// Time       : $O(C^2 \cdot R)$ time where $R$ is rows and $C$ is columns.
+// Space      : $O(R)$ auxiliary space for the prefix frequency hash map.
 // URL        : https://leetcode.com/problems/number-of-submatrices-that-sum-to-target/
 // Synced via : CodeRecall (https://coderecall.vercel.app)
 // ////////////////////////////////////////////////////////////
 
-// Solution for Number of Submatrices That Sum to Target
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 1; j < c; j++) {
+                matrix[i][j] += matrix[i][j - 1];
+            }
+        }
+
+        int count = 0;
+        for (int c1 = 0; c1 < c; c1++) {
+            for (int c2 = c1; c2 < c; c2++) {
+                unordered_map<int, int> mp;
+                mp[0] = 1;
+                int sum = 0;
+
+                for (int row = 0; row < r; row++) {
+                    int val = matrix[row][c2] - (c1 > 0 ? matrix[row][c1 - 1] : 0);
+                    sum += val;
+                    if (mp.find(sum - target) != mp.end()) {
+                        count += mp[sum - target];
+                    }
+                    mp[sum]++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
